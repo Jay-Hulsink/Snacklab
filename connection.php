5c1b111fd34fc8edacb2977a55eaa011cb32c08f
Jay Hulsink <574898@edu.rocmn.nl>
2025-06-20 14:24:07 +0200

    testing query and fetch functionality

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@
 try {
     $connect = new PDO("mysql:host=localhost;dbname=SnackLab", "local_user", "internal");
 
-    $tmp = $connect->query('SELECT * FROM users');
+    $tmp = $connect->query('USE SnackLab; SELECT * FROM users');
     $tmp->fetchAll(PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
