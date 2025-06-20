b312c6e423154a785540c392e0d3d24cadbc6225
Jay Hulsink <574898@edu.rocmn.nl>
2025-06-20 14:06:50 +0200

    some testing of pdo

--- connection.php
+++ connection.php
@@ -1,4 +1,10 @@
 <?php
 
-$connect = new PDO("mysql:host=localhost;dbname=SnackLab", "local_user", "internal");
+try {
+    $connect = new PDO("mysql:host=localhost;dbname=SnackLab", "local_user", "internal");
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    exit;
+}
+
 ?>
