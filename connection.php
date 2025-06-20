1e44d6d52a4c06b16abd2783850858f4535f717b
Jay Hulsink <574898@edu.rocmn.nl>
2025-06-20 11:13:54 +0200

    some testing of pdo

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
 
-$connect = new PDO("mysql:host=127.0.0.1;dbname=SnackLab", "local_user", "internal");
+$connect = new PDO("mysql:host=localhost;dbname=SnackLab", "local_user", "internal");
 ?>
